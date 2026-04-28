443d38cedf2938fedf30634098b6b4895499585f
DariaSluzhyk123 <adventdar31@gmail.com>
2026-04-28 10:54:49 +0200

    Add files via upload

---
+++ wypoz/video.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Video On Demand</title>
+    <link rel ="stylesheet" href="styl3.css">
+</head>
+<body>
+    <header>
+
+    </header>
+    <section id="left">
+        <h1>Internetowa Wypożyczalnia filmów</h1>
+
+       
+</section>
+<section id="right">
+    <table>
+        <tr>
+            <th>Kryminał</th>
+            <th>Horror</th>
+            <th>Przygodowy</th>
+</tr>
+<tr>
+    <td>20</td>
+    <td>30</td>
+    <td>20</td>   
+    </tr>
+</table>
+</section>
+
+<section id = "filmy"class="polecane">
+    <h3>Polecamy</h3>
+</section>
+<section id = "filmy"class="fantastyczne">
+    <h3>f</h3>
+</section>
+
+
+
+<footer></footer>
+</body>
+</html>
