440da9fa1ff4ec6dad40b9f01aaf3b4eae5ff19c
DariaSluzhyk123 <adventdar31@gmail.com>
2026-05-15 13:19:13 +0200

    Add files via upload

---
+++ sandaly/buty/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Obuwie</title>
+    <limk rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Obuwie męskie</h1>
+    </header>
+    <section class ="glowny">
+        <form>
+        <input value="">
+        <iput type="submit" name = "zamów">
+        </form>
+    </section>
+    <footer>
+        <p>Dariia Sluzhynska</p>
+    <footer>
+ 
+
+
+
+
+</body>
+</html>
