44ce926390526731e469862f228e727efea9bf61
DariaSluzhyk123 <adventdar31@gmail.com>
2026-06-08 23:24:12 +0200

    Refactor zdrowia.php to include disease symptoms

--- choroby/zdrowia.php
+++ choroby/zdrowia.php
@@ -7,7 +7,7 @@ $conn = mysqli_connect("localhost", "root", "", "choroby");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wykaz chorób</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styl.css">
 </head>
 <body>
     <header>
@@ -23,26 +23,48 @@ $conn = mysqli_connect("localhost", "root", "", "choroby");
             <h2>Choroby zakaźne</h2>
             <ol>
                 <?php
-
+                    $zapytanie1 = "SELECT nazwa FROM `choroby` WHERE zakazna ='T' GROUP BY(nazwa);";
+                    $wynik = mysqli_query($conn, $zapytanie1);
+                        while($row = mysqli_fetch_row($wynik)){
+                            echo "<li>". $row['0'] ."</li>";
+                    }
                 ?>
             </ol>
         </section>
         <section class="prawa">
             <h2>Objawy chorób</h2>
-            <form action="" method="post">
-            <input type="list">
+            <form method="POST">
+            <select name="choroba">
             <?php
+                $zapytanie2 = "SELECT id, nazwa FROM `choroby`;";
+                $wynik2 = mysqli_query($conn, $zapytanie2);
+                    while($row = mysqli_fetch_row($wynik2)){
+                        echo "<option>". $row['1'] ."</option>";
+                    }
             ?>
-            <input type="submit" name="sprawdź">
-            <?php
-            ?>
-
+            </select>
+            <input type="submit" value="sprawdź">
             </form>
+            <div id="skrypt3">
+                <?php
+                    if(isset($_POST["choroba"])){
+                        $id = $_POST["choroba"];
+                    }
+                    $zapytanie3 = "SELECT objawy.nazwa FROM `objawy` JOIN choroby ON objawy.nazwa = choroby.nazwa WHERE objawy.id = $id;";
+                    $wynik3 = mysqli_query($conn, $zapytanie3);
+                    while($row = mysqli_fetch_row($wynik3)){
+                        echo "<span>". $row['objaw'] ."</span>";
+                    }
+
+                ?>
+            </div>
         </section>
     </main>
     <footer>
         <p>Strone opracował/a: 0000000</p>
     </footer>
+    <img id="zdrowie" src="zdrowia.png" alt="Życzymy zdrowia!">
+
 
 <?php
 $conn->close();
